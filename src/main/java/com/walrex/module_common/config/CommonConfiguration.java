bc67154fb7de3b3776771a748cbaf446ac7b6a29
Ronald E. Aybar D <read424@gmail.com>
2025-06-03 10:45:08 -0500

    Squashed 'module-common/' content from commit 41c25ad
    git-subtree-dir: module-common
    git-subtree-split: 41c25ad123e433b71d5ba08efd5f9463b8137972

---
+++ src/main/java/com/walrex/module_common/config/CommonConfiguration.java
@@ -0,0 +1,11 @@
+package com.walrex.module_common.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+@ComponentScan("com.walrex.module_common")
+public class CommonConfiguration {
+}
