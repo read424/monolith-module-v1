6f1261178d2f98a84e6d4ad2064f9891c5365308
Ronald E. Aybar D <read424@gmail.com>
2025-06-03 11:02:13 -0500

    refactor: remover module-mailing para integración como subtree

--- module-mailing/src/main/java/com/walrex/module_mailing/application/ports/input/MailUseCase.java
+++
@@ -1,8 +0,0 @@
-package com.walrex.module_mailing.application.ports.input;
-
-import com.walrex.module_mailing.domain.model.MailMessage;
-import reactor.core.publisher.Mono;
-
-public interface MailUseCase {
-    Mono<Void> sendMail(MailMessage mailMessage);
-}
--- module-mailing/src/main/java/com/walrex/module_mailing/application/ports/output/MailSenderPort.java
+++
@@ -1,8 +0,0 @@
-package com.walrex.module_mailing.application.ports.output;
-
-import com.walrex.module_mailing.domain.model.MailMessage;
-import reactor.core.publisher.Mono;
-
-public interface MailSenderPort {
-    Mono<Void> send(MailMessage mailMessage);
-}
