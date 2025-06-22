5d8417568d0957e9324fde6508d52b10bd75eedc
Ronald E. Aybar D <read424@gmail.com>
2025-06-22 15:32:17 -0500

    feat: enable cache redis y configuracion application entorno application-prod.yml

--- module-core/src/main/java/com/walrex/module_core/ModuleCoreApplication.java
+++ module-core/src/main/java/com/walrex/module_core/ModuleCoreApplication.java
@@ -2,10 +2,21 @@ package com.walrex.module_core;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = { RedisRepositoriesAutoConfiguration.class})
+@EnableR2dbcRepositories(basePackages = {
+		"com.walrex.gateway.gateway.infrastructure.adapters.outbound.persistence.repository",
+		"com.walrex.user.module_users.infrastructure.adapters.outbound.persistence.repository",
+		"com.walrex.role.module_role.infrastructure.adapters.outbound.persistence.repository",
+		"com.walrex.module_articulos.infrastructure.adapters.outbound.persistence.repository",
+		"com.walrex.module_almacen.infrastructure.adapters.outbound.persistence.repository"
+})
 @ComponentScan(basePackages = {
 		"com.walrex",
 		"com.walrex.module_core",
