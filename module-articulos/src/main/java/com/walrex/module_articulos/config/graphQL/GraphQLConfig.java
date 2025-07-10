f2cd517153668d5e809fb9a7e0683539f202f220
Ronald E. Aybar D <read424@gmail.com>
2025-07-10 14:36:19 -0500

    fix(module-articulos): aplicando type scalar para formatos de fechas

--- module-articulos/src/main/java/com/walrex/module_articulos/config/graphQL/GraphQLConfig.java
+++ module-articulos/src/main/java/com/walrex/module_articulos/config/graphQL/GraphQLConfig.java
@@ -1,6 +1,9 @@
 package com.walrex.module_articulos.config.graphQL;
 
 import graphql.schema.Coercing;
+import graphql.schema.CoercingParseLiteralException;
+import graphql.schema.CoercingParseValueException;
+import graphql.schema.CoercingSerializeException;
 import graphql.schema.GraphQLScalarType;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,23 +21,31 @@ public class GraphQLConfig {
             .scalar(GraphQLScalarType.newScalar()
                 .name("Date")
                 .description("Una fecha en formato ISO (YYYY-MM-DD)")
-                .coercing(new Coercing<Object, Object>() {
+                .coercing(new Coercing<LocalDate, String>() {
                     @Override
-                    public String serialize(Object dataFetcherResult) {
+                    public String serialize(Object dataFetcherResult) throws CoercingSerializeException {
                         if (dataFetcherResult instanceof LocalDate) {
                             return ((LocalDate) dataFetcherResult).format(DateTimeFormatter.ISO_LOCAL_DATE);
                         }
-                        return null;
+                        throw new CoercingSerializeException("Expected LocalDate but got: " + dataFetcherResult.getClass());
                     }
 
                     @Override
-                    public LocalDate parseValue(Object input) {
-                        return LocalDate.parse((String) input, DateTimeFormatter.ISO_LOCAL_DATE);
+                    public LocalDate parseValue(Object input) throws CoercingParseValueException {
+                        try {
+                            return LocalDate.parse((String) input, DateTimeFormatter.ISO_LOCAL_DATE);
+                        } catch (Exception e) {
+                            throw new CoercingParseValueException("Invalid date format: " + input, e);
+                        }
                     }
 
                     @Override
-                    public LocalDate parseLiteral(Object input) {
-                        return LocalDate.parse(input.toString(), DateTimeFormatter.ISO_LOCAL_DATE);
+                    public LocalDate parseLiteral(Object input) throws CoercingParseLiteralException {
+                        try {
+                            return LocalDate.parse(input.toString(), DateTimeFormatter.ISO_LOCAL_DATE);
+                        } catch (Exception e) {
+                            throw new CoercingParseLiteralException("Invalid date format: " + input, e);
+                        }
                     }
                 })
                 .build());
--- module-articulos/src/main/java/com/walrex/module_articulos/domain/model/dto/ArticulosSearchDTO.java
+++ module-articulos/src/main/java/com/walrex/module_articulos/domain/model/dto/ArticulosSearchDTO.java
@@ -2,8 +2,10 @@ package com.walrex.module_articulos.domain.model.dto;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
 @Builder
+@EqualsAndHashCode(callSuper = true)
 public class ArticulosSearchDTO extends ArticuloDto {
 }
