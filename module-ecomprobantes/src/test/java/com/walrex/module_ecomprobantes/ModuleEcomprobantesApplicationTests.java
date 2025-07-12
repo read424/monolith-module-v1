6697ef406a2436419ffcd35a9997e003fb164a62
Ronald E. Aybar D <read424@gmail.com>
2025-07-12 17:32:42 -0500

    feat(ecomprobantes): configuración de testing
    - Estructura de testing para el módulo
    - Configuración para pruebas unitarias e integración
    - Preparado para testing con StepVerifier y WebTestClient

---
+++ module-ecomprobantes/src/test/java/com/walrex/module_ecomprobantes/ModuleEcomprobantesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walrex.module_ecomprobantes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuleEcomprobantesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
