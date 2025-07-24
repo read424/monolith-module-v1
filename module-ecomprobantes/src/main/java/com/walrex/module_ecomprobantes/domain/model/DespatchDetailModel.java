bb74de64389a2f75b926399909a3185bd80b300f
Ronald E. Aybar D <read424@gmail.com>
2025-07-23 21:17:34 -0500

    feat(domain): actualizar DespatchDetailModel con detalles de despacho

--- module-ecomprobantes/src/main/java/com/walrex/module_ecomprobantes/domain/model/DespatchDetailModel.java
+++ module-ecomprobantes/src/main/java/com/walrex/module_ecomprobantes/domain/model/DespatchDetailModel.java
@@ -1,4 +1,17 @@
 package com.walrex.module_ecomprobantes.domain.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class DespatchDetailModel {
+    private String descDetail;
+    private String codDetail;
+    private Double cntDetail;
+    private String uniDetail;
 }
