cfca9d015e69f0b2fe9263060d58107e0afb3c18
Ronald E. Aybar D <read424@gmail.com>
2025-07-23 21:17:22 -0500

    feat(domain): actualizar CompanyModel con información de empresa

--- module-ecomprobantes/src/main/java/com/walrex/module_ecomprobantes/domain/model/CompanyModel.java
+++ module-ecomprobantes/src/main/java/com/walrex/module_ecomprobantes/domain/model/CompanyModel.java
@@ -1,4 +1,15 @@
 package com.walrex.module_ecomprobantes.domain.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class CompanyModel {
+    private String numRuc;
+    private String razonSocial;
 }
