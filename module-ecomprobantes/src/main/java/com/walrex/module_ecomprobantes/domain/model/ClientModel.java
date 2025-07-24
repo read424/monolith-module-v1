4e28597af470ec3c5f436ef865ba40eef98c40e8
Ronald E. Aybar D <read424@gmail.com>
2025-07-23 21:17:11 -0500

    feat(domain): actualizar ClientModel con datos de cliente para guías

--- module-ecomprobantes/src/main/java/com/walrex/module_ecomprobantes/domain/model/ClientModel.java
+++ module-ecomprobantes/src/main/java/com/walrex/module_ecomprobantes/domain/model/ClientModel.java
@@ -1,4 +1,16 @@
 package com.walrex.module_ecomprobantes.domain.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class ClientModel {
+    private String tipDocument;
+    private String numDocument;
+    private String razonSocial;
 }
