dee130dd7f08ad00f2d1f1e1c94fbbb026a0054f
Ronald E. Aybar D <read424@gmail.com>
2025-07-23 21:17:46 -0500

    feat(domain): actualizar DespatchModel con estructura de guía de remisión

--- module-ecomprobantes/src/main/java/com/walrex/module_ecomprobantes/domain/model/DespatchModel.java
+++ module-ecomprobantes/src/main/java/com/walrex/module_ecomprobantes/domain/model/DespatchModel.java
@@ -1,4 +1,22 @@
 package com.walrex.module_ecomprobantes.domain.model;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class DespatchModel {
+    private String numVersion;
+    private String tipoDocumento;
+    private String tipoSerie;
+    private String nroCorrelativo;
+    private LocalDate fecEmision;
+    private CompanyModel company;
+    private ClientModel destinatorio;
+    private ShipmentModel envio;
+    private List<DespatchDetailModel> detalle;
 }
