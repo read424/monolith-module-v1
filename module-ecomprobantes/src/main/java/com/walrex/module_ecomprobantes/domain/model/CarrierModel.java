caad0be25aa3ecb496a111bb8610001c6fc8eb6b
Ronald E. Aybar D <read424@gmail.com>
2025-07-23 21:16:56 -0500

    feat(domain): actualizar CarrierModel con información de transportista

--- module-ecomprobantes/src/main/java/com/walrex/module_ecomprobantes/domain/model/CarrierModel.java
+++ module-ecomprobantes/src/main/java/com/walrex/module_ecomprobantes/domain/model/CarrierModel.java
@@ -1,5 +1,17 @@
-package com.walrex.module_ecomprobantes.domain;
+package com.walrex.module_ecomprobantes.domain.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class CarrierModel {
-
+    private String tipoDocumento;
+    private String numDocumento;
+    private String razonSocial;
+    private String nroMTC;
 }
