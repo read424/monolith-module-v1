88a3547a6b6a3ccab78eefb13a46d5cb658d3ea3
Ronald E. Aybar D <read424@gmail.com>
2025-07-23 21:16:47 -0500

    feat(domain): actualizar AddressModel con nuevos campos para guías de remisión

--- module-ecomprobantes/src/main/java/com/walrex/module_ecomprobantes/domain/model/AddressModel.java
+++ module-ecomprobantes/src/main/java/com/walrex/module_ecomprobantes/domain/model/AddressModel.java
@@ -1,4 +1,17 @@
 package com.walrex.module_ecomprobantes.domain.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class AddressModel {
+    private String ubigeo;
+    private String direccion;
+    private String codLocal;
+    private String numRuc;
 }
