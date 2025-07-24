38260f31cc8ba8abec85f0607c540266f2642ad2
Ronald E. Aybar D <read424@gmail.com>
2025-07-23 21:18:08 -0500

    feat(domain): actualizar ShipmentModel con información de envío

--- module-ecomprobantes/src/main/java/com/walrex/module_ecomprobantes/domain/model/ShipmentModel.java
+++ module-ecomprobantes/src/main/java/com/walrex/module_ecomprobantes/domain/model/ShipmentModel.java
@@ -1,4 +1,23 @@
 package com.walrex.module_ecomprobantes.domain.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class ShipmentModel {
+    private String codTraslado;
+    private String modTraslado;
+    private LocalDate fechaTraslado;
+    private Double pesoTotal;
+    private String unidadPeso;
+    private AddressModel llegada;
+    private AddressModel partida;
+    private CarrierModel transportista;
 }
