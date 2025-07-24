ac450a7f33f15bb8cf2a4cba737757c00f76a68e
Ronald E. Aybar D <read424@gmail.com>
2025-07-23 21:16:13 -0500

    feat(infrastructure): agregar configuraciones para templates y Thymeleaf

---
+++ module-ecomprobantes/src/main/java/com/walrex/module_ecomprobantes/infrastructure/config/EcomprobantesThymeleafConfig.java
@@ -0,0 +1,28 @@
+package com.walrex.module_ecomprobantes.infrastructure.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+
+@Configuration
+public class EcomprobantesThymeleafConfig {
+
+    @Bean
+    public TemplateEngine templateEngine() {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
+        return templateEngine;
+    }
+
+    private ClassLoaderTemplateResolver templateResolver() {
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setPrefix("templates/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        templateResolver.setCacheable(false);
+        return templateResolver;
+    }
+}
