5cab3eb368509b705761a3bcc38ffe4fc0c16459
Ronald E. Aybar D <read424@gmail.com>
2025-07-23 21:18:31 -0500

    feat(domain): actualizar VehicleModel con datos de vehículo

--- module-ecomprobantes/src/main/java/com/walrex/module_ecomprobantes/domain/model/VehicleModel.java
+++ module-ecomprobantes/src/main/java/com/walrex/module_ecomprobantes/domain/model/VehicleModel.java
@@ -1,4 +1,14 @@
 package com.walrex.module_ecomprobantes.domain.model;
 
+import lombok.*;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class VehicleModel {
+    private String numPlaca;
+    private List<VehicleModel> vehicleSeconds;
 }
