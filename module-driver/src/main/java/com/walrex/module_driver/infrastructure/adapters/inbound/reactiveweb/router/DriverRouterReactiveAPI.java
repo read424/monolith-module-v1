6ae800245e83473a7ee86c872bf0d20dab6432f1
Ronald E. Aybar D <read424@gmail.com>
2025-07-24 22:10:38 -0500

    refactor(router): actualizar rutas y logs en DriverRouterReactiveAPI

--- module-driver/src/main/java/com/walrex/module_driver/infrastructure/adapters/inbound/reactiveweb/router/DriverRouterReactiveAPI.java
+++ module-driver/src/main/java/com/walrex/module_driver/infrastructure/adapters/inbound/reactiveweb/router/DriverRouterReactiveAPI.java
@@ -15,34 +15,38 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 @Slf4j
 public class DriverRouterReactiveAPI {
-    private static final String PATH_DRIVER = "driver";
-    private final DriverHandler driverHandler;
+        private static final String PATH_DRIVER = "driver";
+        private final DriverHandler driverHandler;
 
-    @Bean
-    public RouterFunction<ServerResponse> driverRoutes() {
-        return RouterFunctions.route()
-                .path("/" + PATH_DRIVER, builder -> builder
-                        .POST("/", RequestPredicates.accept(MediaType.APPLICATION_JSON), driverHandler::createDriver)
-                        .GET("/search", RequestPredicates.accept(MediaType.APPLICATION_JSON),
-                                driverHandler::buscarConductor)
-                        .PUT("/{id}", RequestPredicates.accept(MediaType.APPLICATION_JSON), driverHandler::updateDriver)
-                        .DELETE("/{id}", RequestPredicates.accept(MediaType.APPLICATION_JSON),
-                                driverHandler::deleteDriver)
-                        .GET("/{id}", RequestPredicates.accept(MediaType.APPLICATION_JSON), driverHandler::getDriver))
-                .before(request -> {
-                    log.info("🔄 Router {} recibió solicitud: {} {}", PATH_DRIVER,
-                            request.method(), request.path());
-                    return request;
-                }).after((request, response) -> {
-                    log.info("✅ Router {} respondió a: {} {} con estado: {}", PATH_DRIVER,
-                            request.method(), request.path(), response.statusCode());
-                    return response;
-                })
-                .build();
-    }
+        @Bean
+        public RouterFunction<ServerResponse> driverRoutes() {
+                return RouterFunctions.route()
+                                .path("/" + PATH_DRIVER, builder -> builder
+                                                .POST("/create", RequestPredicates.accept(MediaType.APPLICATION_JSON),
+                                                                driverHandler::createDriver)
+                                                .GET("/search", RequestPredicates.accept(MediaType.APPLICATION_JSON),
+                                                                driverHandler::buscarConductor)
+                                                .PUT("/{id}", RequestPredicates.accept(MediaType.APPLICATION_JSON),
+                                                                driverHandler::updateDriver)
+                                                .DELETE("/{id}", RequestPredicates.accept(MediaType.APPLICATION_JSON),
+                                                                driverHandler::deleteDriver)
+                                                .GET("/{id}", RequestPredicates.accept(MediaType.APPLICATION_JSON),
+                                                                driverHandler::getDriver))
+                                .before(request -> {
+                                        log.info("🔄 Router {} recibió solicitud: {} {}", PATH_DRIVER,
+                                                        request.method(), request.path());
+                                        return request;
+                                }).after((request, response) -> {
+                                        log.info("✅ Router {} respondió a: {} {} con estado: {}", PATH_DRIVER,
+                                                        request.method(), request.path(), response.statusCode());
+                                        return response;
+                                })
+                                .build();
+        }
 
-    @PostConstruct
-    public void init() {
-        log.info("🔌 Rutas del módulo de driver registradas en: /{}", PATH_DRIVER);
-    }
+        @PostConstruct
+        public void init() {
+                System.out.println("🔌 Rutas del módulo de driver registradas en: /" + PATH_DRIVER);
+                log.info("🔌 Rutas del módulo de driver registradas en: /{}", PATH_DRIVER);
+        }
 }
