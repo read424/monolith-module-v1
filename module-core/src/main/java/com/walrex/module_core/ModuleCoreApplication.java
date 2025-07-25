c2204b91630d3e5b376f494462fed699e621ccb4
Ronald E. Aybar D <read424@gmail.com>
2025-07-24 22:22:44 -0500

    fix(core): agregar rutas de module_driver en configuración principal

--- module-core/src/main/java/com/walrex/module_core/ModuleCoreApplication.java
+++ module-core/src/main/java/com/walrex/module_core/ModuleCoreApplication.java
@@ -18,7 +18,8 @@ import lombok.extern.slf4j.Slf4j;
         "com.walrex.role.module_role.infrastructure.adapters.outbound.persistence.repository",
         "com.walrex.module_articulos.infrastructure.adapters.outbound.persistence.repository",
         "com.walrex.module_almacen.infrastructure.adapters.outbound.persistence.repository",
-        "com.walrex.module_ecomprobantes.infrastructure.adapters.outbound.persistence.repository"
+        "com.walrex.module_ecomprobantes.infrastructure.adapters.outbound.persistence.repository",
+        "com.walrex.module_driver.infrastructure.adapters.outbound.persistence.repository"
 })
 @ComponentScan(basePackages = {
         "com.walrex",
@@ -28,7 +29,8 @@ import lombok.extern.slf4j.Slf4j;
         "com.walrex.module_common",
         "com.walrex.user",
         "com.walrex.role",
-        "com.walrex.module_ecomprobantes"
+        "com.walrex.module_ecomprobantes",
+        "com.walrex.module_driver"
 })
 @Slf4j
 public class ModuleCoreApplication {
