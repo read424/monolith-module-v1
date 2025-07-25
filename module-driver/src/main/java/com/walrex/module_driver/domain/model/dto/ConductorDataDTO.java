553a774d3cdc1996d0d79855cce0f63c9053fd6d
Ronald E. Aybar D <read424@gmail.com>
2025-07-24 22:07:25 -0500

    refactor(domain): agregar idConductor a ConductorDataDTO

--- module-driver/src/main/java/com/walrex/module_driver/domain/model/dto/ConductorDataDTO.java
+++ module-driver/src/main/java/com/walrex/module_driver/domain/model/dto/ConductorDataDTO.java
@@ -7,6 +7,7 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ConductorDataDTO {
+    private Long idConductor;
     private TipoDocumentoDTO tipoDocumento;
     private String numeroDocumento;
     private String apellidos;
