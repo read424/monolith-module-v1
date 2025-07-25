3bef02a7d3f6e5bf6960ded031a0f7b58ee2be36
Ronald E. Aybar D <read424@gmail.com>
2025-07-24 22:08:28 -0500

    fix(mapper): mapear nombres y apellidos en DriverDomainMapper

--- module-driver/src/main/java/com/walrex/module_driver/domain/model/mapper/DriverDomainMapper.java
+++ module-driver/src/main/java/com/walrex/module_driver/domain/model/mapper/DriverDomainMapper.java
@@ -7,5 +7,8 @@ import com.walrex.module_driver.domain.model.dto.CreateDriverDTO;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING, unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface DriverDomainMapper {
-    CreateDriverDTO toDTO(DriverDomain createDriverDTO);
+
+    @Mapping(source = "lastName", target = "apellidos")
+    @Mapping(source = "firstName", target = "nombres")
+    CreateDriverDTO toDTO(DriverDomain driverDomain);
 }
