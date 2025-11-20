191802aa09dd0d56a3f1daf3d5bf40843194477b
Ronald E. Aybar D <read424@gmail.com>
2025-11-19 20:51:19 -0500

    test(module-liquidaciones): agregar test de contexto de aplicación
    Test base que verifica la carga correcta del contexto
    Spring del módulo liquidaciones

---
+++ module-liquidaciones/src/test/java/com/walrex/despacho/module_liquidaciones/ModuleLiquidacionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walrex.despacho.module_liquidaciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuleLiquidacionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
