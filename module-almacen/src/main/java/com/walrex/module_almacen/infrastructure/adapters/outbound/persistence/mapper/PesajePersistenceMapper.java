9beae0c21b3e4894b715b842b81b93c955f64fbb
Ronald E. Aybar D <read424@gmail.com>
2026-02-20 19:56:03 -0500

    feat(almacen): update PesajePersistenceMapper for session mapping

--- module-almacen/src/main/java/com/walrex/module_almacen/infrastructure/adapters/outbound/persistence/mapper/PesajePersistenceMapper.java
+++ module-almacen/src/main/java/com/walrex/module_almacen/infrastructure/adapters/outbound/persistence/mapper/PesajePersistenceMapper.java
@@ -4,17 +4,19 @@ import com.walrex.module_almacen.domain.model.PesajeDetalle;
 import com.walrex.module_almacen.infrastructure.adapters.outbound.persistence.projection.SessionPesajeActivaWithDetailProjection;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface PesajePersistenceMapper {
 
     @Mapping(target = "id_detordeningresopeso", ignore = true)
-    @Mapping(target = "id_ordeningreso", source = "id_ordeningreso")
+    @Mapping(target = "idOrdenIngreso", source = "idOrdenIngreso")
     @Mapping(target = "peso_rollo", ignore = true)
-    @Mapping(target = "cod_rollo", expression = "java(projection.getLote() + \"-\" + (projection.getCnt_registro() + 1))")
-    @Mapping(target = "cnt_registrados", source = "cnt_registro")
+    @Mapping(target = "cod_rollo", ignore = true)
+    @Mapping(target = "cnt_registrados", source = "cntRegistro")
     @Mapping(target = "completado", ignore = true)
-    @Mapping(target = "id_detordeningreso", source = "id_detordeningreso")
+    @Mapping(target = "cnt_rollos", source = "cntRollos")
+    @Mapping(target = "id_detordeningreso", source = "idDetOrdenIngreso")
     @Mapping(target = "id_session_hidden", source = "id")
     PesajeDetalle toDomain(SessionPesajeActivaWithDetailProjection projection);
 }
