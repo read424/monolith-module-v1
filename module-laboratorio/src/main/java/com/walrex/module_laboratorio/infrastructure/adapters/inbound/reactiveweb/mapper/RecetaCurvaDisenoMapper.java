bc5692b3d25675ba1863039339a315e6398144db
Ronald E. Aybar D <read424@gmail.com>
2026-05-18 09:24:21 -0500

    feat(laboratorio): actualizar mapper RecetaCurvaDisenoMapper

--- module-laboratorio/src/main/java/com/walrex/module_laboratorio/infrastructure/adapters/inbound/reactiveweb/mapper/RecetaCurvaDisenoMapper.java
+++ module-laboratorio/src/main/java/com/walrex/module_laboratorio/infrastructure/adapters/inbound/reactiveweb/mapper/RecetaCurvaDisenoMapper.java
@@ -1,6 +1,8 @@
 package com.walrex.module_laboratorio.infrastructure.adapters.inbound.reactiveweb.mapper;
 
+import com.walrex.module_laboratorio.domain.model.CurvaDisenoItem;
 import com.walrex.module_laboratorio.domain.model.Receta;
+import com.walrex.module_laboratorio.infrastructure.adapters.inbound.reactiveweb.dto.response.CurvaDisenoItemResponse;
 import com.walrex.module_laboratorio.infrastructure.adapters.inbound.reactiveweb.dto.response.RecetaCurvaDisenoResponse;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants;
@@ -8,4 +10,5 @@ import org.mapstruct.MappingConstants;
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface RecetaCurvaDisenoMapper {
     RecetaCurvaDisenoResponse toResponse(Receta receta);
+    CurvaDisenoItemResponse toItemResponse(CurvaDisenoItem item);
 }
